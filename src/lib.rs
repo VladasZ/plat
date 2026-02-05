c53828b8aae0e1cdf71554ede873e7dd00a3bfba
Vladas Zakrevskis <146100@gmail.com>
2026-02-05 12:07:56 +0200

    better mobile and desktop

--- src/lib.rs
+++ src/lib.rs
@@ -2,24 +2,24 @@ pub struct Platform;
 
 impl Platform {
     pub const MAC: bool = cfg!(target_os = "macos");
-    pub const WIN: bool = cfg!(target_os = "windows");
     pub const LINUX: bool = cfg!(target_os = "linux");
+    pub const WINDOWS: bool = cfg!(target_os = "windows");
     pub const IOS: bool = cfg!(target_os = "ios");
     pub const ANDROID: bool = cfg!(target_os = "android");
+    pub const DESKTOP: bool = Self::MAC || Self::LINUX || Self::WINDOWS;
     pub const MOBILE: bool = Self::IOS || Self::ANDROID;
-    pub const DESKTOP: bool = !Self::MOBILE;
     pub const WASM: bool = cfg!(target_arch = "wasm32");
 }
 
 impl Platform {
     pub fn dump() {
         dbg!(Self::MAC);
-        dbg!(Self::WIN);
         dbg!(Self::LINUX);
+        dbg!(Self::WINDOWS);
         dbg!(Self::IOS);
         dbg!(Self::ANDROID);
-        dbg!(Self::MOBILE);
         dbg!(Self::DESKTOP);
+        dbg!(Self::MOBILE);
         dbg!(Self::WASM);
     }
 }
@@ -37,9 +37,10 @@ pub fn platforms() {
 
         macos:   { target_os = "macos" },
         linux:   { target_os = "linux" },
+        windows: { target_os = "windows" },
 
-        mobile:  { any(    target_os = "android", target_os = "ios")  },
-        desktop: { not(any(target_os = "android", target_os = "ios")) },
+        desktop: { any(target_os =   "macos", target_os = "linux", target_os = "windows") },
+        mobile:  { any(target_os = "android", target_os =   "ios"                       ) },
     }
 }
 
